24d5f63a5877ab64b15f2a778bf5140639cc0f93
Alexandr <nuvorish81@mail.ru>
2020-10-23 20:50:58 +0600

    добавленна функция -  После отправки формы -  показать на странице сообщение, что цитата добавлена, или ошибки;

--- source/api/urls.py
+++ source/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import QuoteViewSet
-
+from .views import QuoteViewSet, get_token_view
 
 router = DefaultRouter()
 router.register('quote', QuoteViewSet, basename='quote')
@@ -11,5 +10,6 @@ app_name = 'api'
 
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('get-token/', get_token_view, name='get_token'),
 ]
